b03003fbd148d2537cf7d6d7cc7c64e3ea5cae1a
Mari <mariab1@list.ru>
2022-09-01 20:11:08 +0300

    fix problem

--- src/test/java/ru/netology/ConditionalApplicationTests.java
+++ src/test/java/ru/netology/ConditionalApplicationTests.java
@@ -1,13 +1,32 @@
 package ru.netology;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConditionalApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+    public static GenericContainer<?> devContainer = new GenericContainer<>("devapp").withExposedPorts(8080);
+    public static GenericContainer<?> prodContainer = new GenericContainer<>("prodapp").withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devContainer.start();
+        prodContainer.start();
+    }
 
     @Test
     void contextLoads() {
+        System.out.println(devContainer.getMappedPort(8080));
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + 8080, String.class);
+        System.out.println(forEntity.getBody());
     }
 
 }
