680d6680cc28b02d82ff7589c5202e8665a91c0e
Mari <mariab1@list.ru>
2022-09-01 21:01:14 +0300

    tests added

--- src/test/java/ru/netology/ConditionalApplicationTests.java
+++ src/test/java/ru/netology/ConditionalApplicationTests.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +24,14 @@ class ConditionalApplicationTests {
     }
 
     @Test
-    void contextLoads() {
-        System.out.println(devContainer.getMappedPort(8080));
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + 8080, String.class);
-        System.out.println(forEntity.getBody());
+    void devTest() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + devContainer.getMappedPort(8080) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is dev", forEntity.getBody());
     }
 
+    @Test
+    void prodTest() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + prodContainer.getMappedPort(8081) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is production", forEntity.getBody());
+    }
 }
